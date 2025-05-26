1ab9a7a830a2f2d0513d8d6e6c98947a63995dd3
Clément <clementmoisan@gmail.com>
2025-05-26 21:45:10 +0200

    pom.xml

--- Java_SAE/src/main/java/fr/univ_orleans/iut45/r207/tp3/App.java
+++ Java_SAE/src/main/java/fr/univ_orleans/iut45/r207/tp3/App.java
@@ -17,33 +17,7 @@ import java.util.Map;
  */
 public class App {
 	public static Graph<String, DefaultEdge> collaborationsJsonToGraph(String jsonFilePath) throws IOException {
-		Gson gson = new Gson();
-		Type listType = new TypeToken<List<Map<String, Object>>>(){}.getType();
-		List<Map<String, Object>> collaborations;
-		try (FileReader reader = new FileReader(jsonFilePath)) {
-			collaborations = gson.fromJson(reader, listType);
-		}
-
-		Graph<String, DefaultEdge> graph = GraphTypeBuilder
-				.directed()
-				.allowingMultipleEdges(true)
-				.allowingSelfLoops(true)
-				.vertexSupplier(SupplierUtil.createStringSupplier())
-				.edgeSupplier(SupplierUtil.createDefaultEdgeSupplier())
-				.buildGraph();
-
-		for (Map<String, Object> collaboration : collaborations) {
-			String source = (String) collaboration.get("source");
-			String target = (String) collaboration.get("target");
-			if (!graph.containsVertex(source)) {
-				graph.addVertex(source);
-			}
-			if (!graph.containsVertex(target)) {
-				graph.addVertex(target);
-			}
-			graph.addEdge(source, target);
-		}
-		return graph;
+		
 	}
 	public static void main(String[] args) {
 		
